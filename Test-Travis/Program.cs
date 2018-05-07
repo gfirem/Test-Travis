efed7f10232854f012d9294a28767b02f055a82d
Guillermo Figueroa <gfirem@gmail.com>
2018-05-07 14:06:22 -0300

    init commit

---
+++ Test-Travis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestTravis
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace Test
+{
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+			int Value = 60;
+			Assert.IsTrue(Value > 50);
+        }
+    }
+}
